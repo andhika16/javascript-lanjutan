a66b1394b47de645baf6d8c5be1f519f2d87ed66
moh.andhika16@gmail.com <andhika16>
2020-05-31 20:29:40 +0700

    menambahkan index.html

--- script.js
+++ script.js
@@ -11,3 +11,27 @@ for (let i = 0; i < 8; i++) {
 
 
 }
+
+
+
+function Car(speed, color, horsepower) {
+    this.speed = speed;
+    this.color = color;
+    this.horsepower = horsepower;
+
+
+    function speedActivate(speed) {
+        this.speed += 1;
+        console.log("kecepatan bertambah" + this.speed + "KM");
+
+    }
+
+
+
+}
+
+
+let ferrari = new Car;
+
+Car.speedActivate(200);
+
