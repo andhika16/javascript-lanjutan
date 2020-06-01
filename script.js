686920cf502097298b9643765c214a5d7605b014
andhika16 <moh.andhika16@gmail.com>
2020-06-01 11:23:33 +0700

    Object.create

--- script.js
+++ script.js
@@ -1,33 +1,82 @@
-function Car(speed, color, hp, fuel) {
 
-    this.speed = speed;
-    this.color = color;
-    this.hp = hp;
-    this.fuel = fuel;
+const car_method = {
 
 
-    this.carSpeed = function (extendSpeed) {
+    car_speed: function (km) {
 
-        this.speed = this.speed + extendSpeed;
+        this.speed = km * 2;
+        this.fuel -= km;
+        console.log(`car is accelerate : ${this.speed} km/h`);
 
-
-
-
-        console.log("kecepatan mobil bertambah menjadi " + this.speed + " KM/hrs")
 
     }
-
 
 
 }
 
 
 
-let ferrari = new Car(200, "red", 1000);
+function Car(speed, fuel) {
 
-ferrari.carSpeed(6);
+    let car = Object.create(car_method);
+
+    car.speed = speed;
+    car.fuel = fuel;
 
 
+    return Car;
+}
+
+
+
+let ferrari = Car(2, 10);
+
+
+
+
+const mahasiswaMethod = {
+
+    makan: function (porsi) {
+
+        this.energi += porsi;
+        console.log(`Halo ${this.nama}, Selamat Makan`);
+
+
+    },
+
+    main: function (jam) {
+
+        this.energi -= jam;
+        console.log(`halo ! ${this.nama}, Selamat Bermain`);
+    },
+
+    tidur: function (jam) {
+
+        this.energi += jam * 2;
+        console.log(`Halo ${this.nama}, Selamat tidur`);
+
+
+    }
+
+
+}
+
+
+function Mahasiswa(nama, energi) {
+
+    let mahasiswa = Object.create(mahasiswaMethod);
+
+    mahasiswa.nama = nama;
+    mahasiswa.energi = energi;
+
+    return mahasiswa;
+}
+
+
+
+let andhika = Mahasiswa("andhika", 20);
+
+let kope = Mahasiswa("kope", 20);
 
 
 
