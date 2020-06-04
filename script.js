96fa7d3e50fc288392d462be38758dd0016952ca
andhika16 <moh.andhika16@gmail.com>
2020-06-04 20:26:42 +0700

    Arrow function II

--- script.js
+++ script.js
@@ -1,24 +1,50 @@
-let mahasiswa = ["codeigniter", "react", "bootstrap"];
 
-// dibawah ini contoh arrow function
-let huruf = mahasiswa.map(nama => ({ nama, nomor: nama.length }));
-let contoh = mahasiswa.map(nama => nama.length);
-console.table(huruf);
+// let Mahasiswa = function () {
 
-
-console.log(contoh);
+//     this.nama = "andhika";
+//     this.umur = 1000;
 
 
+//     sayHello = function () {
+//         console.log(`hallo, ${this.nama} , umur kamu adalah${this.umur}`);
+
+//     }
+
+
+//     setInterval(() => {
+
+//         console.log(this.umur -= 1); jika fungsi disamping ini menggunakan function declaration maka this pada fungsi sebelah akan mengacu ke window.
+//     }, 1000);
+
+
+// }
 
 
 
+// let andhika = new Mahasiswa();
+
+const box = document.querySelector('.box');
 
 
+box.addEventListener('click', function () {
+
+    let satu = 'size';
+    let dua = 'caption';
+
+    if (this.classList.contains(satu)) {
+
+        [satu, dua] = [dua, satu];
+
+    }
+
+    this.classList.toggle(satu);
+
+    setTimeout(() => {
+        this.classList.toggle(dua);
+    }, 500);
 
 
-
-
-
+});
 
 
 
