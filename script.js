0f0a131cf72669f956373d732c59c6ad4010103f
andhika16 <moh.andhika16@gmail.com>
2020-06-07 11:43:47 +0700

    update IV

--- script.js
+++ script.js
@@ -1,34 +1,12 @@
-
-// Template literals
-
-
-// Embeded Expressions
-
-const x = prompt('masukkan nilai');
-let test = ` ${x} apakah nilai disamping ganjil atau genap?  ${(x % 2 == 1 ? 'nilai disamping adalah ganjil' : 'nilai disamping adalah genap')}`
+const nama = 'muhammad andhika';
+const umur = 20;
 
 
 
-// HTML fragments 
-const mhs = {
-
-    nama: "muhammad andhika",
-    nim: 19533080,
-    jurusan: "Teknik Informatika"
-
-
+function test(string) {
+    return string
 }
 
 
-let cetak = `<div class="ya">
-
-    <h1>Nama saya Adalah ${this.nama}</h1>
-
-    <p class="">Nim saya Adalah ${this.nim}</p>
-
-</div>`;
-
-
-console.log(test);
-
+let str = test`halo nama saya ${nama}, umur saya ${umur}, tahun`;
 
