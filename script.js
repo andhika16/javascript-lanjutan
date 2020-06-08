a368a21eded75f753c84c85309b3d2c1d2ae1b04
andhika16 <moh.andhika16@gmail.com>
2020-06-08 14:44:49 +0700

    for of... for in

--- script.js
+++ script.js
@@ -1,70 +1,57 @@
-
-// // Destructring function
-
-function kalkulasi(a, b) {
-
-    return [a * b, a + b, a / b, a - b];
-
-}
+const nama = ['dhika', 'zulkfili', 'doddy'];
 
 
-const [kali, tambah, bagi, kurang] = kalkulasi(20, 3)
+for (n of nama) {
 
-console.log(kali);
-
-
-
-
-// // jika ingin menambah properti tidak urut dengan returnya harus menggunakan objek seperti dibawah ini 
-
-function hitung(a, b) {
-
-    return {
-        kali: a * b,
-        tambah: a + b,
-        bagi: a / b,
-        kurang: a - b
-    }
+    console.log(n);
 
 }
 
 
 
-const { kali, kurang, bagi, tambah } = hitung(2, 3);
+function jumlahAngka() {
+
+    console.log(arguments);
 
 
-console.log(kurang);
+}
 
-// // Destructuring object 
 
-const mhs1 = {
-    nama: 'moh andhika',
+jumlahAngka(1,24,532344,35)
+
+
+
+
+let mhs = {
+    nama: "andhika",
     umur: 33,
-    email: 'moh.andhika16@gmail.com',
-    nilai: {
+    email: "moh.andhika16@gmail.com"
+};
 
-        tugas: 80,
-        uas: 70,
-        uts: 75
+for (n in mhs) {
+    console.log(mhs[n]);
+}
+
+function angka() {
+
+    let jumlah = 0;
+    for ( a of arguments){
+
+      jumlah += a
+    }
+    return jumlah;
+}
+console.log(angka(1,2,3,4,5,6,7,8));
+
+function add () {
+    let nama = [];
+    for ( n of arguments){
+
+        nama += n
 
     }
-
+    return nama;
 }
 
-
-function hai({ nama, umur, nilai: { tugas, uts, uas } }) {
-
-    return `Halo nama saya ${nama}, umur saya ${umur}, nilai uas saya ${tugas}`;
-}
-
-
-console.log(hai(mhs1));
-
-
-
-
-
-
-
-
+console.log(add('y','o', 'l', 'o'))
 
