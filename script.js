c1b7925c74a1590f41a5f43ecdb44c2fa63f1fd6
andhika16 <moh.andhika16@gmail.com>
2020-06-12 10:46:26 +0700

    fetch

--- script.js
+++ script.js
@@ -28,47 +28,81 @@
 //     }
 //     , () => { });
 
-$('.click-button').on('click', function () {
-    $.ajax({
-        url: `http://www.omdbapi.com/?apikey=83626b40&s=${$('.input-keyword').val()}`,
-        success: results => {
-            const movies = results.Search;
+// $('.click-button').on('click', function () {
+//     $.ajax({
+//         url: `http://www.omdbapi.com/?apikey=83626b40&s=${$('.input-keyword').val()}`,
+//         success: results => {
+//             const movies = results.Search;
+//             let cards = '';
+//             movies.forEach((m) => {
+//                 cards += showMovie(m);
+//             });
+
+//             $('.movie-container').html(cards);
+// // ketika tombol detail diklik
+//             $('.modal-detail-button').on('click', function () {
+
+//                 $.ajax({
+//                     url: ` http://www.omdbapi.com/?apikey=83626b40&i=${$(this).data('imdbid')}`,
+//                     success: m => {
+//                         const movieDetail = ShowMovieDetail(m);
+//                         $('.modal-body').html(movieDetail);
+//                     },
+//                     error: e => console.log(e.responseText)
+//                 });
+
+
+//             });
+
+
+
+//         },
+//         error: e => console.log(e.responseText)
+
+//     });
+
+// });
+
+
+const searchButton = document.querySelector('.click-button');
+searchButton.addEventListener('click', function () {
+    const inputKey = document.querySelector('.input-keyword');
+    fetch('http://www.omdbapi.com/?apikey=83626b40&s=' + inputKey.value)
+        .then(res => res.json())
+        .then(res => {
+            const movie = res.Search;
             let cards = '';
-            movies.forEach((m) => {
-                cards += showMovie(m);
-            });
+            movie.forEach(m => cards += showMovie(m));
+            const movContain = document.querySelector('.movie-container');
+            movContain.innerHTML = cards;
 
-            $('.movie-container').html(cards);
+            // ketika tombol see detail di klik
+            const detailButton = document.querySelectorAll('.modal-detail-button');
+            detailButton.forEach(btn => {
+                btn.addEventListener('click', function () {
+                    fetch(`http://www.omdbapi.com/?apikey=83626b40&i=${$(this).data('imdbid')}`)
+                        .then(res => res.json())
+                        .then(movie => {
+                            const movieDetail = ShowMovieDetail(movie);
+                            const modalBody = document.querySelector('.modal-body');
+                            modalBody.innerHTML = movieDetail;
+                        })
+                        .catch(reject => console.log(reject))
 
-            $('.modal-detail-button').on('click', function () {
 
-                $.ajax({
-                    url: ` http://www.omdbapi.com/?apikey=83626b40&i=${$(this).data('imdbid')}`,
-                    success: m => {
-                        const movieDetail = ShowMovieDetail(m);
-                        $('.modal-body').html(movieDetail);
-                    },
-                    error: e => console.log(e.responseText)
                 });
-
-
             });
 
-
-
-        },
-        error: e => console.log(e.responseText)
-
-    });
-
+        })
 });
 
 
 
 
 
+
 function showMovie(m) {
-    return `<div class="col-md-4 my-3">
+    return `<div class="col-md-4 my-3 sm12">
                         <div class="card" >
                             <img src="${m.Poster}" class="card-img-top">
                             <div class="card-body">
@@ -90,10 +124,10 @@ function ShowMovieDetail(m) {
                        </div>
                        <div class="col">
                            <ul class="list-group">
-                               <li class="list-group-item">Title ${m.Title}</li>
+                               <li class="list-group-item"> <strong class="">Title</strong>  ${m.Title}</li>
                                <li class="list-group-item"> <strong>Director :</strong> ${m.Director} </li>
                                <li class="list-group-item"> <strong>Actors:</strong> ${m.Actors}</li>
-                               <li class="list-group-item">Year: ${m.Year}</li>
+                               <li class="list-group-item"><strong class="">Year:</strong> ${m.Year}</li>
                                <li class="list-group-item"> <p>Plot:${m.Plot}</p> </li>
                            </ul>
                        </div>
